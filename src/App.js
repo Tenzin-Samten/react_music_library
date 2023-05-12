a52f1e30789e20fe268d4b34053e64c13cbcb3d0
Tenzin Paldon <tenzinpaldon@Tenzins-iMac.lan>
2023-05-12 18:58:18 -0400

    music_library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Gallery from './Componenets/Gallery'
+import Searchbar from './Componenets/Searchbar'
+import ArtistView from "./Componenets/ArtistView";
+import AlbumView from "./Componenets/AlbumView";
 
 function App() {
+  const [search, setSearch] = useState('') 
+  const [message, setMessage] =useState('Search for Music!')
+  const [data, setData] = useState([])
+
+  const API_URL = `https://itunes.apple.com/search?term=`
+
+  useEffect(() => {
+    if(search) {
+   const fetchData = async () => {
+    const URL = encodeURI(API_URL + search)
+    console.log(URL)
+    const response = await fetch (URL) 
+    const data = await response.json() 
+    console.log(data)
+   if (data.results.length > 0) {
+    setData(data.results)
+  } else {
+    setMessage('Not Found')
+  }
+   } 
+
+   fetchData() 
+  }
+}, [search])
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearch(term)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      
+      {message}
+      <Router>
+        <Routes>
+          <Route path='/' elements={
+            <>
+            <Searchbar handleSearch ={handleSearch} />
+            <Gallery data={data}/>
+            </>
+          } />
+          <Route path='/album/:id' element={<AlbumView />} />
+          <Route path='/artist/:id' element={<ArtistView />} />
+      </Routes>
+      </Router>
+      </div>
   );
 }
 
